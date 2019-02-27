91742b27e1ad35985893c30889d6f66977988ff0
Nishant Mehta <nish03+github@gmail.com>
2019-02-27 13:51:14 -0500

    api enhancements

--- bootstrap-identity/kerberos/kerberos-server/generate-kcc.sh
+++ bootstrap-identity/kerberos/kerberos-server/generate-kcc.sh
@@ -3,7 +3,9 @@
 cd /shared-location
 
 while true;do
-    echo "secret" | /usr/bin/kinit admin/admin@kerberos.gomesh.com -c /shared-location/cache.txt
+    echo "secret" | /usr/bin/kinit admin/admin@kerberos.gomesh.com 
+    cat /tmp/krb5cc_0 | base64 > /shared-location/cache.txt
+    cp /etc/krb5.conf /shared-location/
     echo `date` >> /shared-location/last_generated.txt
     sleep 10m 
 done
--- bootstrap-identity/kerberos/rest-api/app.py
+++ bootstrap-identity/kerberos/rest-api/app.py
@@ -1,16 +1,38 @@
 from flask import Flask
 from flask_restful import Resource, Api
-
 app = Flask(__name__)
 api = Api(app)
+import base64
+
+class FetchKRB5(Resource):
+    def get(self):
+        with open("/kerberos-server/krb5.conf", "rb") as image_file:
+              encoded_string = image_file.read()
+              result =  {
+                  "Krb5Conf": base64.encodestring(encoded_string).replace("\n", "")
+              
+              }
+              return result
 
 class FetchKCC(Resource):
     def get(self):
-        with open("/kerberos-server/cache.txt", "rb") as image_file:
+        with open("/kerberos-server/cache.txt", "r") as image_file:
             encoded_string = image_file.read()
-            return encoded_string.encode("base64")
+            result =  {
+                "KerberosCredentialCache": encoded_string.replace("\n", "")
 
-api.add_resource(FetchKCC, '/api/kerberos/fetch')
+            }
+            return result
+    def post(self):
+        with open("/kerberos-server/cache.txt", "r") as image_file:
+            encoded_string = image_file.read()
+            result =  {
+                "KerberosCredentialCache": encoded_string.replace("\n", "") 
+
+            }   
+            return result 
+api.add_resource(FetchKCC, '/api/kerberos/fetch', methods = ['GET', 'POST', 'DELETE'])
+api.add_resource(FetchKRB5, '/api/kerberos/config/fetch', methods = ['GET'])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
